932332945edc4fa98d683250231c1717f913a034
Islamshaban98 <islamshaba998@gmail.com>
2021-01-25 09:23:45 +0200

    adding aboutMe component

---
+++ src/component/AboutMe/AboutMe.js
@@ -0,0 +1,9 @@
+import React from "react";
+import useStyles from "./AboutMe.style";
+
+const AboutMe = () => {
+  const classes = useStyles();
+  return <></>;
+};
+
+export default AboutMe;
---
+++ src/component/AboutMe/AboutMe.style.js
@@ -0,0 +1,4 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(() => {});
+export default useStyles;
